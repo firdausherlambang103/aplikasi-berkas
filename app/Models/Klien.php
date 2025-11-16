c6c35da9d1db4337879b29fb36ca06dec42c3de5
firdausherlambang103 <firdausherlambang18@gmail.com>
2025-11-16 20:10:43 +0700

    update terbaru 16/11/25 20.11

--- app/Models/Klien.php
+++ app/Models/Klien.php
@@ -1,14 +1,21 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// PERBAIKAN: Tambahkan "extends Model"
 class Klien extends Model
 {
     use HasFactory;
-    protected $table = 'klien';
-    protected $fillable = ['kode_klien', 'nama_klien', 'nomer_wa'];
+    
+    protected $fillable = [
+        'kode_klien',
+        'nama_klien',
+        'nomer_wa',
+        'alamat',
+    ];
 
     public function berkas()
     {
